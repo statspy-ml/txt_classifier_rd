fae356e38519876be78dd19a955d4b78030cae5e
Rodrigo Toledo <rodrigotoledo@MacBook-Pro-de-Rodrigo.local>
2023-09-25 14:26:31 -0300

    initial commit

---
+++ topics.py
@@ -0,0 +1,3 @@
+RH_TOPICS = ['salário', 'remuneração', ' benefícios', 'treinamentos', 'hora-extra']
+
+MULTI_TOPICS = ['emabalagem avariado', 'tempo de entrega', 'preço do frete', 'produto estragado']
